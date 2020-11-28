ff4b5eedf6684d471a36ad8fd25cb94a72b5157c
Max Kimlyk <maxskater3@yandex.ru>
2020-11-28 21:58:52 +0300

    ssh port hint

--- bot/views/status.py
+++ bot/views/status.py
@@ -8,15 +8,20 @@ def build_response(status: types.Status, config: Dict[str, Any]) -> Tuple[str, s
     else:
         prefix = ''
 
+    if 'ssh_port_hint' in config:
+        port = '-p {}'.format(config['ssh_port_hint'])
+    else:
+        port = ''
+
     time_text = status.time.isoformat()
 
     text = (
         'Server ip: <b>{ip}</b>\n'
         'Updated at (UTC): {time}\n'
         '\n'
-        'SSH to host: '
-        '<code>ssh {prefix}{ip}</code>'
-    ).format(ip=status.ip, time=time_text, prefix=prefix)
+        'SSH to host: \n'
+        '<code>ssh {port} {prefix}{ip}</code>'
+    ).format(ip=status.ip, port=port, time=time_text, prefix=prefix)
 
     return text, 'HTML'
 
